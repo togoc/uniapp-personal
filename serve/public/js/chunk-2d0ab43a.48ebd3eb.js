4f0ee6aa2b6af95f74213c487022dd637c43a8c9
togoc <309128090@qq.com>
2020-05-01 18:08:15 +0800

    index item line-height

--- serve/public/js/chunk-2d0ab43a.48ebd3eb.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ab43a"],{1511:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" user ")])},a=[],c={},l=c,r=t("2877"),s=Object(r["a"])(l,u,a,!1,null,null,null);e["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0ab43a.48ebd3eb.js.map
