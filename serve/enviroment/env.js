ff08e998f32bcb223e15667352a4666ec7ac0ad2
togoc <309128090@qq.com>
2020-06-01 22:47:31 +0800

    移除集群 socket通讯需要处理

--- serve/enviroment/env.js
+++ serve/enviroment/env.js
@@ -1,10 +1,10 @@
 const path = require('path')
 module.exports = {
-    key: process.env.KEY || "1234",
-    newKey: process.env.NEW_KEY || "1234",
-    password: process.env.PASSWORD || "1234",
-    root: process.env.ROOT || path.resolve(__dirname, '../'),
-    url: process.env.URL || 'localhost',
-    fullUrl: process.env.FULLURL || 'http://localhost',
-    mongoURL: process.env.MONGODB_URL
+  key: process.env.KEY || "1234",
+  newKey: process.env.NEW_KEY || "1234",
+  salt: process.env.SALT || "1234",
+  root: process.env.ROOT || path.resolve(__dirname, '../'),
+  url: process.env.URL || 'localhost',
+  fullUrl: process.env.FULLURL || 'http://localhost',
+  mongoURL: process.env.MONGODB_URL
 }
