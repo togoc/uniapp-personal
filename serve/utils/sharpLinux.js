fde157cb4f14cc0d34cc92ac7f033c9752772d69
togoc <309128090@qq.com>
2020-06-11 14:09:15 +0800

    修改sharp

--- serve/utils/sharpLinux.js
+++ serve/utils/sharpLinux.js
@@ -1,18 +1,16 @@
 const path = require('path')
 const fs = require('fs')
-const express = require('express')
-const app = express()
 
+const serve = require('http').createServer((req, res) => {
+    if (req.url === '/download-sharp/libvips-8.9.1-linux-x64.tar.gz') {
+        let readStream = fs.createReadStream(path.resolve(__dirname, 'sharp', 'libvips-8.9.1-linux-x64.tar.gz'))
+        res.writeHead(200, { 'Content-Disposition': 'attachment; filename="' + 'libvips-8.9.1-linux-x64.tar.gz' + '"' });
+        readStream.pipe(res)
+    } else {
+        res.end('npm config set sharp_dist_base_url "http://localhost:8787/download-sharp/');
+    }
+})
 
-
-app.get('/download-sharp/libvips-8.9.1-linux-x64.tar.gz', (req, res) => {
-  let readStream = fs.createReadStream(path.resolve(__dirname, 'sharp', 'libvips-8.9.1-linux-x64.tar.gz'))
-  res.set('Content-Disposition', 'attachment; filename="' + 'libvips-8.9.1-linux-x64.tar.gz' + '"');
-  readStream.pipe(res)
-});
-
-
-
-app.listen(8787, () => {
-  console.log(`Server started on port`);
-});
+serve.listen(8787, () => {
+    console.log(`npm config set sharp_dist_base_url "http://localhost:8787/download-sharp/`)
+})
